1edc3695f7565bf70c2e99f74388ea9e53004cf7
Evgenie <nestroykozni@gmail.com>
2026-01-22 15:07:35 +0700

    Implement user input for currency conversion and define conversion function in main.go

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,9 +2,30 @@ package main
 
 import "fmt"
 
+const usdToEuro = 0.94
+const usdToRub = 80.0
+
+func getParameters() (float64, string, string) {
+	var sourceCurrency string
+	var targetCurrency string
+	var amount float64
+
+	fmt.Println("__ Конвертер валюты __")
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&sourceCurrency)
+	fmt.Print("Введите целевую валюту: ")
+	fmt.Scan(&targetCurrency)
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+
+	return amount, sourceCurrency, targetCurrency
+}
+
+func convertCurrency(amount float64, sourceCurrency string, targetCurrency string) float64 {
+
+}
+
 func main() {
-	const usdToEuro = 0.94
-	const usdToRub = 80.0
 	const euroToRub = usdToRub / usdToEuro
 	fmt.Printf("Course euro to rub equals %.2f\n", euroToRub)
 }
