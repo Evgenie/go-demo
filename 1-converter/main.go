01613edc66637da1e1c4229dc44ebe90f086b7f0
Evgenie <nestroykozni@gmail.com>
2026-01-24 16:52:46 +0700

    Refactor currency converter in main.go to improve user input handling and conversion logic. Added functions for source and target currency selection, and enhanced amount input validation. Updated convertCurrency function to print conversion results directly.

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,31 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
 
-const usdToEuro = 0.94
+const currencies = "usd/eur/rub"
+const usdToEur = 0.94
 const usdToRub = 80.0
+var currencyArr = strings.Split(currencies, "/")
+var eurToUsd = 1 / usdToEur
+var eurToRub = usdToRub / usdToEur
+var rubToUsd = 1 / usdToRub
+var rubToEur = 1 / eurToRub
+
+func getSourceCurrency() string {
+	var sourceCurrency string
+	fmt.Printf("Введите исходную валюту(%s): ", currencies)
+	fmt.Scan(&sourceCurrency)
+	return sourceCurrency
+}
+func getTargetCurrency(suggestCurrencies string) string {
+	var targetCurrency string
+	fmt.Printf("Введите целевую валюту(%s): ", suggestCurrencies)
+	fmt.Scan(&targetCurrency)
+	return targetCurrency
+}
+func getAmount() (float64, error) {
+	var amount float64
+	fmt.Printf("Введите сумму: ")
+	_, error := fmt.Scan(&amount)
+	if error != nil {
+		return -1, error
+	}
+	return amount, nil
+}
 
 func getParameters() (float64, string, string) {
-	var sourceCurrency string
-	var targetCurrency string
-	var amount float64
-
-	fmt.Println("__ Конвертер валюты __")
-	fmt.Print("Введите исходную валюту: ")
-	fmt.Scan(&sourceCurrency)
-	fmt.Print("Введите целевую валюту: ")
-	fmt.Scan(&targetCurrency)
-	fmt.Print("Введите сумму: ")
-	fmt.Scan(&amount)
-
+	sourceCurrency := getSourceCurrency()
+	for !slices.Contains(currencyArr, sourceCurrency) {
+		fmt.Println("Ошибка. Повторите ввод")
+		sourceCurrency = getSourceCurrency()
+	}
+	sourceCurrencyIdx := slices.Index(currencyArr, sourceCurrency)
+	suggestCurrencyArr := slices.Delete(currencyArr, sourceCurrencyIdx, sourceCurrencyIdx + 1)
+	suggestCurrencies := strings.Join(suggestCurrencyArr, "/")
+	targetCurrency := getTargetCurrency(suggestCurrencies)
+	for !slices.Contains(suggestCurrencyArr, targetCurrency) {
+		fmt.Println("Ошибка. Повторите ввод")
+		targetCurrency = getTargetCurrency(suggestCurrencies)
+	}
+	amount, err := getAmount()
+	for amount <= 0 || err != nil {
+		fmt.Println("Ошибка. Повторите ввод")
+		getAmount()
+	}
+	
 	return amount, sourceCurrency, targetCurrency
 }
 
-func convertCurrency(amount float64, sourceCurrency string, targetCurrency string) float64 {
-	return 0.0
+func convertCurrency(amount float64, sourceCurrency string, targetCurrency string) {
+	var converted float64
+	switch sourceCurrency {
+		case "usd":
+			if targetCurrency == "rub" {
+				converted = amount * usdToRub
+				} else {
+				converted = amount * usdToEur
+			}
+		case "eur":
+			if targetCurrency == "rub" {
+				converted = amount * eurToRub
+				} else {
+				converted = amount * eurToUsd
+			}
+		default:
+			if targetCurrency == "usd" {
+				converted = amount * rubToUsd
+			} else {
+				converted = amount * rubToEur
+			}
+	}
+	fmt.Printf("Course %s to %s equals %.2f\n",sourceCurrency, targetCurrency,  converted)
 }
 
 func main() {
-	const euroToRub = usdToRub / usdToEuro
-	fmt.Printf("Course euro to rub equals %.2f\n", euroToRub)
+	fmt.Println("__ Конвертер валюты __")
+	convertCurrency(getParameters())
 }
